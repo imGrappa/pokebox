1f99aec6da3b3de4cd06aa6cb598630c36ce8f90
Koray <75887457+imGrappa@users.noreply.github.com>
2024-02-10 00:03:23 +0300

    Update to search for Pokemon by name and type

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,12 @@
+// Pokebox Search Input, Poke Button and Poke-wrapper div
 const pokeInput = document.querySelector(".poke-input")
 const searchBtn = document.querySelector(".poke-btn")
 const pokeWrapper = document.querySelector(".poke-wrapper .row")
 
+// the number of pokemon we want to print
 const pokeCount = 20
 
+// Background colors according to Pokemon types
 const pokeColors = {
     fire:"#fddfdf",
     grass:"#defde0",
@@ -22,12 +25,14 @@ const pokeColors = {
     ice:"#e0f5ff",
 }
 
+//a piece of code that allows us to call async according to the pokemon count
 const initPokemon = async () => {
     for (let i = 1; i <= pokeCount; i++) {
         await getPokemon(i)
     }
 }
 
+//a piece of code that allows us to call pokemon data as api and process it as json
 const getPokemon = async (id) => {
     let url = `https://pokeapi.co/api/v2/pokemon/${id}`
     let res = await fetch(url)
@@ -35,9 +40,15 @@ const getPokemon = async (id) => {
     createPokemonBox(data)
 }
 
+
 const createPokemonBox = (pokemon) => {
+    //code that magnifies the first letter of pokemon names
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+
+    //code that converts the incoming pokemon id to string data and adds two zeros at the beginning
     const id = pokemon.id.toString().padStart(3,'0')
+
+    //code to select the pokemon type on the data we receive
     const type = pokemon.types[0].type.name
     const color = pokeColors[type]
     const weight = pokemon.weight
@@ -45,8 +56,7 @@ const createPokemonBox = (pokemon) => {
     const pokeBoxEl = document.createElement('div')
     pokeBoxEl.classList.add('col-lg-3')
     pokeBoxEl.innerHTML = ` 
-    <div class="poke-border">
-            <div class="poke-item text-center" style="background-color:${color}">
+        <div class="poke-item text-center" style="background-color:${color}">
             <div class="poke-img">
                 <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png" alt="${name} image">
             </div>
@@ -54,8 +64,7 @@ const createPokemonBox = (pokemon) => {
             <div class="poke-id">#${id}</div>
             <div class="poke-weight">${weight} KG</div>
             <div class="poke-type">Type: ${type}</div>
-        </div>      
-    </div>
+        </div> 
     `
     pokeWrapper.appendChild(pokeBoxEl)
 }
@@ -64,5 +73,16 @@ initPokemon()
 
 pokeInput.addEventListener('input', function(e) {
     const search = pokeInput.value.toLowerCase();
-    console.log(search);
-})
+    const pokeNames = document.querySelectorAll('.poke-name');
+    const types = document.querySelectorAll('.poke-type');
+
+    // Check Pokemon names and types
+    pokeNames.forEach((pokeName, index) => {
+        const type = types[index].textContent.toLowerCase();
+        if (!pokeName.innerHTML.toLowerCase().includes(search) && !type.includes(search)) {
+            pokeName.parentElement.parentElement.style.display = 'none';
+        } else {
+            pokeName.parentElement.parentElement.style.display = 'block';
+        }
+    });
+});
